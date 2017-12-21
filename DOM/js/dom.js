2ddf2a11bbca37aaa4c571f376546e1f607d3790
zhaoyuanyuan <swpu_zyy@163.com>
2017-12-21 15:37:45 +0800

    init Dom

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -1,2 +1,24 @@
 obj.setAttribute('属性', '属性的值');  //更改某个元素的属性值
 obj.getAttribute('属性');     //获得某个元素的属性值
+创建元素节点  ele.createElement(" ")
+创建文本节点 ele.createTextNode(" ")
+添加孩子  element.appendChild(newChild)
+插入节点 targetNode.parentNode.insertBefore(newNode,targetNode)
+删除节点  targetNode.parentNode.removeChild(targetNode)
+替换节点  reference = ele.replaceChild(newChild,oldChild)  //ele节点里要有oldChild节点
+复制节点，创建副本，返回指向该副本的新指针 reference = node.cloneNode(bealoon) //若为true，则连同node里面的子节点一同复制，若为false，则只复制元素节点（包括属性节点，不包括文本节点）
+检查是否有子元素 bealoonValue = ele.hasChildNodes//检查一个元素是否含有子元素，若有子元素则返回true，文本节点和属性节点永远返回false
+
+node.nodeName  只读属性，返回元素的值，若为元素节点，返回节点元素相当于tagName，若为属性节点，则返回属性名字，若为文本节点，则返回文本值 
+nodeType
+1  元素节点
+2  属性值
+3  文本值
+
+nodeValue  读写属性，文本值（属性和文本节点，元素节点返回null）
+parentNode 父节点
+childNodes 返回一个数组，给定元素孩子节点组成
+firstChild
+lastChild
+nextSibling  下一个元素
+previousSibling  上一个元素
--- DOM/js/index.js
+++ DOM/js/index.js
@@ -1,148 +1,93 @@
-//增加window加载函数
-//当有多个函数需要加载时
-function addLoadEvent(func){
-    var oldload= window.onload;
-    if(typeof window.onload!='function'){
-        window.onload=func;
+
+//图片显示函数
+function showPic(whichPic){
+    var placeholder = document.getElementById("placeholder"); //占位图片 
+    var source = whichPic.getAttribute("href");   //源图片链接
+    placeholder.setAttribute("src",source);  //图片替换
+
+    var description = document.getElementById("description");  
+    var text = whichPic.getAttribute('title')?  whichPic.getAttribute('title') :" ";   //源图片描述
+    description.childNodes[0].nodeValue = text;   //描述替换
+    
+    return false;
+    // var body = document.body  ;// var body = document.getElementsByTagName("body")[0]  获得body对象里面的所有内容
+    //var element = document.body.childNodes;   //获得标签节点
+    //console.log( element );
+}
+//点击加载图片
+function gallery(){
+    if(!document.getElementById) return false;
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(i=0; i<links.length; i++ ){
+        links[i].onclick = function() {
+           return  showPic(this);
+        }
+        links[i].onkeypress = links[i].onclick;
+    }
+}
+addLoad( gallery );
+
+
+window.onload=function ()
+{
+    var oUl=document.getElementById('ul1');
+    
+    //IE6-8
+    //alert(oUl.childNodes.length);
+    for(var i=0;i<oUl.childNodes.length;i++)
+    {
+        //nodeType==3   ->  文本节点
+        //nodeType==1   ->  元素节点
+        //alert(oUl.childNodes[i].nodeType);
+        if(oUl.childNodes[i].nodeType==1)  
+        {
+            oUl.childNodes[i].style.background='red';  //子节点
+        }
+    }
+    alert(oUl.children.length);   //子节点
+
+    //IE6-8
+    //oUl.firstChild.style.background='red'; 获取第一个孩子
+    //高级浏览器
+    //oUl.firstElementChild.style.background='red';
+    if(oUl.firstElementChild)
+    {
+        oUl.firstElementChild.style.background='red';
+    }
+    else
+    {
+        oUl.firstChild.style.background='red';
+    }
+    alert(oUl.parentNode);  // 父节点
+};
+
+
+
+
+//脚本库
+
+//window加载多个函数
+function addLoad(func){
+    var oldload = window.onload;
+    if(typeof window.onload != "function" ){
+        window.onload = func ;
     }else{
-        window.onload=function(){
+        window.onload = function() {
             oldload();
             func();
         }
     }
 }
 
-
-//在已有的元素后面插入一个新的元素  由于没有定义方法，所以需要自己写
-function insertAfter(newelement,targetelement){
-    var parent= targetelement.parentNode;  //获取目标父元素
-    if(parent.lastChild==targetelement){
-        parent.appendChild(newelement);
+//在元素后面添加子元素
+function insertAfter(newElement,targetElement){
+    var parent = targetElement.parentNode;
+    if( parent.lastChild==targetElement ){
+        parent.appendChild(newElement);
     }else{
-        parent.insertBefore(newelement,targetelement.nextSibling);
+        parent.insertBefore(newElement,targetElement.nextSilbing);
     }
 }
-
-function preparePlaceholer(){
-    var placeholder= document.createElement("img");
-    placeholder.setAttribute("src","images/spaider_man.jpg");
-    placeholder.setAttribute("alt","siderman");
-    var description1= document.createElement("p");
-    var text1=document.createTextNode("11111111111");
-    description1.appendChild(text1);
-    var gallery = document.getElementById("imagegallery");
-    insertAfter(placeholder,gallery);
-    insertAfter(description1,placeholder);
-  /*  gallery.parentNode.insertBefore(placehold,gallery);//第一个参数是插入的，第二个参数是目标元素即你想把新的元素插入到哪个元素前面
-*/
-}
-
-function preparegallery(){
-    var gallery = document.getElementById("imagegallery");
-    var links= gallery.getElementsByTagName("a");
-    var len= links.length;
-    var i;
-    alert(len);
-    for(i=0;i<len;i++){
-        links[i].onclick=function(){
-            return showPic(this)? false:true;
-        };
-        links[i].onkeypress=links[i].onclick;
-    }
-}
-
-function showPic(which){
-    var placehold=document.getElementById("placehold");
-    var href=which.getAttribute("href");
-    placehold.setAttribute("src",href);
-    if(document.getElementById("description")){
-        var text=which.getAttribute("title")?which.getAttribute("title"):"";
-        var description=document.getElementById("description");
-        if(  description.childNodes[0].nodeType==3){
-            description.firstChild.nodeValue =text;
-        }
-    }
-    return true;
-}im
-
-addLoadEvent(preparePlaceholer);
-addLoadEvent(preparegallery);
-
-function addClass(element,value){
-    if(!element.className){
-        element.className=value;
-    }else{
-        newClassName = element.className;
-        newClassName+= " ";
-        newClassName+=value;
-        element.className=newClassName;
-    }
-}
-function styleElementSiblings(tag,theClass){
-    if(!document.getElementsByTagName)return false;
-    var elems = document.getElementsByTagName(tag);
-    var elem;
-    for(var i=0;i<elems.length;i++){
-        elem = elems[i].nextSibling;
-        addClass(elem,theClass);
-    }
-}
-
-addLoadEvent(function(){
-    styleElementSiblings("tr","odd");
-});
-
-/*
-function stripeTables(){
-    if(!document.getElementsByTagName) return false;
-    var tables = document.getElementsByTagName("table");
-    var odd,rows;
-    for(var i=0;i<tables.length;i++){
-        odd=false;
-        rows = tables[i].getElementsByTagName("tr");
-        for(var j=0;j<rows.length;j++){
-            if(odd==true){
-                addClass(rows[j],"odd");
-                odd=false;
-            }else{
-                odd=true;
-            }
-        }
-    }
-}addLoadEvent(stripeTables);*/
-
-
-function positionMessage(){
-    var elem = document.getElementById("message");
-
-    elem.style.position="absolute";
-    elem.style.left="600px";
-    elem.style.top = "700px";
-    movement = setTimeout("moveMessage()",5000);
-}
-function moveMessage(){
-    var elem = document.getElementById("message");
-   var xpos=parseInt(elem.style.left);
-    var ypos=parseInt(elem.style.top);
-    if(xpos==200&&ypos==100){
-        return true;
-    }
-    if(xpos<200){
-        xpos++;
-    }
-    if(xpos>200){
-        xpos--;
-    }
-    if(ypos<100)
-    {
-        ypos++;
-    }
-    if(ypos>100)
-    {
-        ypos--;
-    }
-    elem.style.left = xpos+"px";
-    elem.style.top = ypos+"px";
-    movement = setTimeout("moveMessage()",1000);
-}
-addLoadEvent(positionMessage);
